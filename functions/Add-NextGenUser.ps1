a19e3883bde30b2ea26d64dccf14a5baae41846b
Mike Hokanson <mhokanson@gmail.com>
2023-01-27 17:10:48 -0800

    Move password hashing to its own function

--- functions/Add-NextGenUser.ps1
+++ functions/Add-NextGenUser.ps1
@@ -43,26 +43,7 @@ function Add-NextGenUser {
 
 	# Support -WhatIf output
 	if ($PSCmdlet.ShouldProcess("$($moduleVars.database)","Creating user: $username")) {
-
-		$insecure_password = [Net.NetworkCredential]::new('', $password).Password
-		$salted_string = $username.ToLower() + "nghash" + $insecure_password
-		
-		# Create a hash for the password value
-		$mystream = [IO.MemoryStream]::new([byte[]][char[]]$salted_string)
-		$hashString = $(Get-FileHash -InputStream $mystream -Algorithm SHA1).Hash
-		
-		$ngpwdhash = ""
-		# NextGen drops leading zeros from each byte of the hashed password
-		for($i = 0; $i -lt $hashString.Length; $i++){
-			$byteValue = $hashString.Substring($i,2)
-			if($byteValue.Substring(0,1) -eq "0"){
-				$ngpwdhash += $byteValue.Substring(1,1)
-			} else {
-				$ngpwdhash += $byteValue
-			}
-		
-			$i = $i + 1
-		}
+		$ngpwdhash = Get-NextGenPwdHash $password
 
 		$user_mstr_insert_query = @"
 		INSERT INTO user_mstr
---
+++ functions/Get-NextGenPwdHash.ps1
@@ -0,0 +1,28 @@
+function Get-NextGenPwdHash {
+	[cmdletbinding(SupportsShouldProcess)]
+	Param(
+		[parameter(Mandatory = $true)] [SecureString]$password
+	)
+
+	$insecure_password = [Net.NetworkCredential]::new('', $password).Password
+	$salted_string = $username.ToLower() + $moduleVars.passwordSalt + $insecure_password
+	
+	# Create a hash for the password value
+	$mystream = [IO.MemoryStream]::new([byte[]][char[]]$salted_string)
+	$hashString = $(Get-FileHash -InputStream $mystream -Algorithm SHA1).Hash
+	
+	$ngpwdhash = ""
+	# NextGen drops leading zeros from each byte of the hashed password
+	for($i = 0; $i -lt $hashString.Length; $i++){
+		$byteValue = $hashString.Substring($i,2)
+		if($byteValue.Substring(0,1) -eq "0"){
+			$ngpwdhash += $byteValue.Substring(1,1)
+		} else {
+			$ngpwdhash += $byteValue
+		}
+	
+		$i = $i + 1
+	}
+
+	return $ngpwdhash
+}
